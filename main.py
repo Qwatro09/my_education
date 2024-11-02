14d6d6afbd3e8619a8c00f2393101e4897cff884
Andrei <andre_ws@rambler.ru>
2024-11-02 12:58:47 +0300

    proba

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+# 1st program
+
+print(9 ** 0.5 * 5) # приоритет вначале степень потом умножение
+
+
+# 2st program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4st program
+
+x_sym = '123.456'
+
+print(int(float(x_sym) * 10 % 10))
