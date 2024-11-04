505598fdc4b357cec1f326faa064e3273c958215
AndreSap <andrepq71q@gmail.com>
2024-11-04 13:14:33 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+number_of_completed_lessons = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_lessons
+
+
+print('Курс: ', course_name, ', всего задач: ', number_of_completed_lessons,
+      ', затрачено часов: ', number_of_hours_spent,
+      ', среднее время выполнения: ', time_for_one_task, 'часа.')
+
