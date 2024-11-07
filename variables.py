d0b5d3ade82dd8ec7afec8c34e25d364f3f17260
Андрей <andrepq71q@gmail.com>
2024-11-07 13:53:24 +0300

    correct

--- variables.py
+++ variables.py
@@ -3,7 +3,6 @@ number_of_hours_spent = 1.5
 course_name = 'Python'
 time_for_one_task = number_of_hours_spent / number_of_completed_lessons
 
-
 print(f"Курс: {course_name}, всего задач:{number_of_completed_lessons}, "
       f"затрачено часов: {number_of_hours_spent}, "
       f"среднее время выполнения {time_for_one_task} часа.")
