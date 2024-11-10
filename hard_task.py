2c3d30e8e24c756696f89dc1b1953ccb3d86b05f
Андрей <andrepq71q@gmail.com>
2024-11-10 21:07:00 +0300

    corrected

--- hard_task.py
+++ hard_task.py
@@ -5,13 +5,13 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2],
           [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-list_students = list(students) # переводим множество в список и сортируем его
+list_students = list(students)
 list_students.sort()
 
-for i_num in grades: # ищем среднее арифметическое оценок и сохраняем его в отдельный список
+for i_num in grades:
     i_sum = (sum(i_num) / len(i_num))
     list_grades.append(i_sum)
 
-for j_num in range(len(list_grades)): # создаем словарь из двух списков
+for j_num in range(len(list_grades)):
     my_answer.update({list_students[j_num]:list_grades[j_num]})
 print(my_answer)
